76c1c3a3e915cd3ccb8cec1ec3c28372f65e8526
Franklin Stennett <franklinstennett@gmail.com>
2026-06-21 19:41:06 -0600

    Agregue la logica para la suma

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello world!")
+numero_uno = int(input("Ingresa el primer digito"))
+numero_dos = int(input("Ingresa el segundo digito"))
+suma = numero_dos + numero_uno
+print("La suma de los dos digitos es:", suma)
