317cbf4f053eeec3df15878e924ce56b68110cfb
Franklin Stennett <franklinstennett@gmail.com>
2026-06-21 19:47:25 -0600

    Ya termine mi trabajo, cree una funcion que suma dos digitos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numero_uno = int(input("Ingresa el primer digito"))
-numero_dos = int(input("Ingresa el segundo digito"))
-suma = numero_dos + numero_uno
-print("La suma de los dos digitos es:", suma)
+from suma import suma
+
+suma()
+print("Hello world!")
